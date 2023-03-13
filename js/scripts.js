035540ab0dbfa356aeddd10e591d452ae5a5109d
jepalfer <jepalfer2000@gmail.com>
2023-03-13 17:27:14 +0100

    changed css

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,8 @@
 const commentSection = document.getElementById('comment-section');
-const commentButton = document.getElementById('comment-section.button');
+const commentButton = document.getElementById('comment-section-button');
+
+function myFunction(){
+    commentSection.textContent += 'hola';
+}
+
+commentButton.addEventListener('click', myFunction);
