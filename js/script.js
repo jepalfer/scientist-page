2103c43db7d0823acf8057961032a8104035ea77
jepalfer <jepalfer2000@gmail.com>
2023-03-15 02:25:18 +0100

    Changed html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const boton = document.getElementById('comment-section-button');
 const seccionComentarios = document.getElementById('comment-section');
+const comentario = document.getElementById('comment-input');
 
+const censoredWords = ['patata', 'avion', 'piano', 'censura', 'idiota', 'prueba', 'lorem', 'raton', 'section', 'sangre'];
 
 function hideSection(){
     if (seccionComentarios.style.visibility=='hidden'){
@@ -9,4 +11,10 @@ function hideSection(){
     else
         seccionComentarios.style.visibility='hidden';
 }
+
+function censoreContent(){
+    
+}
+
 boton.addEventListener('click', hideSection);
+comentario.addEventListener('input', censoreContent);
